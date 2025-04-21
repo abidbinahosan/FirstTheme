da6eefa5581440a4963772078205d292d161e9f2
abid-bin-ahosan <abid.ahosan@brainstation-23.com>
2025-04-21 14:00:19 +0600

    Solved Uninstall problem (changed on SchemaMigration.cs)

---
+++ Plugins/Nop.Plugin.Misc.Supplier/Infrastructure/SupplierWidgetZones.cs
@@ -0,0 +1,7 @@
+﻿namespace Nop.Plugin.Misc.Suppliers.Infrastructure
+{
+    public static class SupplierWidgetZones
+    {
+        public const string SupplierDetailsBlock = "admin_supplier_details_block";
+    }
+}
